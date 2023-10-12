9bace5bc7d27664174062f226cfd5c1487ff5f28
Anis <contact.anisujjaman@gmail.com>
2023-10-12 18:22:52 +0600

    setting update

--- config/settings.py
+++ config/settings.py
@@ -20,10 +20,13 @@ env = environ.Env(
 )
 # Set the project base directory
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
